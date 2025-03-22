15784fb1551915f02f083190db593ccc2288a35d
tetiana zorii <tanyusha.zoriy@gmail.com>
2025-03-21 22:59:01 -0400

    complete hw

---
+++ index.js
@@ -0,0 +1,48 @@
+import { program } from "commander";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "./contacts.js";
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+// Функція для виклику відповідного методу з contacts.js
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contactsList = await listContacts();
+      console.table(contactsList);
+      break;
+
+    case "get":
+      const oneContactById = await getContactById(id);
+      console.table(oneContactById);
+      break;
+
+    case "add":
+      const newContact = await addContact({ name, email, phone });
+      console.table(newContact);
+      break;
+
+    case "remove":
+      const deleteContact = await removeContact(id);
+      console.table(deleteContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
